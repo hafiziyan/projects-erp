02a15914e4ac341af5a175a045f783e240c954a6
hafiziyan <mailendra22@gmail.com>
2026-04-13 10:54:54 +0700

    update controller.auth.ts

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -40,7 +40,9 @@ const forgotPasswordSchema = z.object({
 });
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
